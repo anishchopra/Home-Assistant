a5133c0a416883e1638616d7c89df1cad3a9f2d4
Gregory Desrosiers <gdesrosi@uwaterloo.ca>
2017-10-27 20:56:23 -0400

    Added functions to scenemessages

--- raspberry_pi/scenemessages.py
+++ raspberry_pi/scenemessages.py
@@ -1,4 +1,10 @@
 TURNED_OFF_MESSAGE = "turned off"
 TURNED_ON_MESSAGE = "turned on"
-FAN_MESSAGE = "The fan is now "
-LIGHT_MESSAGE = "The light is now "
+FAN_MESSAGE = "the fan"
+LIGHT_MESSAGE = "the light"
+
+def set_light_state_message(room, state):
+    return "Okay, I have " + state + " " + LIGHT_MESSAGE + " in " + room + "."
+
+def set_fan_state_message(room, state):
+    return "Okay, I have " + state + " " + FAN_MESSAGE + " in " + room + "."
