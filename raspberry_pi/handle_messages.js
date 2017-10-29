1bb7292a8e769d6448c78b9ae3413dd2f7a6f1cc
Jason Milasincic <jmilasin@uwaterloo.ca>
2017-10-28 23:03:41 -0400

    Firebase setup

---
+++ raspberry_pi/handle_messages.js
@@ -0,0 +1,41 @@
+var admin = require("firebase-admin");
+var fs = require("fs")
+
+admin.initializeApp({
+	credential: admin.credential.cert("firebase.json"),
+	databaseURL: "https://team-protege.firebaseio.com"
+});
+
+var db = admin.database();
+var messageRef = db.ref('messages')
+
+messageRef.on('child_added', function(data) {
+	var message = data.val().message 
+	var sender = data.val().sender
+
+	var filename = sender + ".json"
+
+	var requestObj = {"interface": "messenger",
+					"messenger_id": sender,
+					"message": message}
+
+	fs.writeFile(filename, JSON.stringify(requestObj), function (err) {
+  		if (err) throw err;
+	});
+
+	console.log(sender);
+
+	var exec = require('child_process').exec;
+	var cmd = 'python3 jarvis.py ' + filename
+
+	console.log(cmd)
+
+	exec(cmd, function(error, stdout, stderr) {
+			// command output is in stdout
+		console.log(error)
+		console.log(stdout)
+		console.log(stderr)
+	});
+
+	data.ref.remove()
+});
---
+++ raspberry_pi/respond.js
@@ -0,0 +1,45 @@
+var admin = require("firebase-admin");
+
+admin.initializeApp({
+	credential: admin.credential.cert("firebase.json"),
+	databaseURL: "https://team-protege.firebaseio.com"
+});
+
+var db = admin.database();
+var responseRef = db.ref('responses')
+
+sender = process.argv[2]
+quick_replies_num = parseInt(process.argv[3])
+
+response = "";
+quick_replies = [];
+
+var i=4;
+for (var j=0; j < quick_replies_num; j++) {
+	reply = {content_type: 'text', title: process.argv[i], payload: process.argv[i+1]};
+	quick_replies.push(reply);
+	i+=2
+}
+
+for (; i<process.argv.length; i++) {
+	response += process.argv[i];
+
+	if (i < process.argv.length - 1) {
+		response += ' ';
+	}
+}
+
+var newResponseRef = responseRef.push();
+
+if (quick_replies.length > 0) {
+	newResponseRef.set({"message": response, "sender": sender, "quick_replies": quick_replies}).then(function(res) {
+		process.exit();
+	});
+}
+else {
+	newResponseRef.set({"message": response, "sender": sender}).then(function(res) {
+		process.exit();
+	});
+}
+
+
